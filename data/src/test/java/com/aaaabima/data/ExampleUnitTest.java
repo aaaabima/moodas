4019fcda9e70e9be44c1b6f83a446c90a4b68d2d
aaaabima <aaaabim@gmail.com>
2022-11-30 19:16:52 +0700

    Clean architecture implementation and dependency centralization

---
+++ data/src/test/java/com/aaaabima/data/ExampleUnitTest.java
@@ -0,0 +1,23 @@
+/*
+ * DANA.id
+ * PT. Espay Debit Indonesia Koe.
+ * Copyright (c) 2017-2022 All Rights Reserved.
+ */
+
+package com.aaaabima.data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
